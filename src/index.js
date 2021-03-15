dcc43192d4357b076b825c035746ae03504e0dd7
miaklwalker <ouisihai@gmail.com>
2021-03-14 23:21:18 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const config = require('../slappey.json');
+const dotenv = require("dotenv").config();
 const client = new Client();
 
 (async () => {
@@ -10,6 +11,6 @@ const client = new Client();
   client.prefix = config.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(config.token);
+  await client.login(process.env.DISC_TOKEN);
 })();
 
