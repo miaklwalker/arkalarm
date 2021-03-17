b33f5af2dbbf25a5febb7e5dfc0d421b582e4275
miaklwalker <ouisihai@gmail.com>
2021-03-17 13:41:25 -0500

    Added the Existing Channel Filter

--- src/commands/setup/SetupCommand.js
+++ src/commands/setup/SetupCommand.js
@@ -22,21 +22,26 @@ let configTemplate = `{
      tribemates:[],
   }
 }`;
-//todo if the server already has ark-alarm channels dont delete.
+
 //todo delete ark alarms that no longer have configs
 
-async function fetchConfig(client){
+function findChannelByName(name,client){
   // Gets all of the channels from Discord Server
   let values = Object.values(client.channels);
   // Verifies That we have correctly gathered the channels
   let channels = values[1] instanceof values[0] && values[1];
-  let config;
+  let target;
   // Goes through each channel and finds the config channel
   channels.forEach(channel=>{
-    if(channel.name === "config"){
-      config = channel;
+    if(channel.name === name){
+      target = channel;
     }
   });
+  return target;
+}
+
+async function fetchConfig(client){
+  let config = findChannelByName("config",client);
   if(config){
     let configMessage;
     let messages =  await config.messages.fetch();
@@ -64,14 +69,15 @@ async function createConfig(message){
   When you are finished filling out the form use the ** !A config **  
   command again to set up your bot!`);
 }
+
 const clusterName = (config) => (`ark-alarm-${config
     .replace(/ /g, '-')
     .toLowerCase()
 }`);
 
-function createChannelsFromConfig(message,config) {
+function createChannelsFromConfig(message,config,client) {
   Object.keys(config).forEach(name => {
-    if (name) {
+    if (name && !findChannelByName(clusterName(name),client)) {
       createChannel(message, clusterName(name), "text")
     }
   });
@@ -91,18 +97,19 @@ module.exports = class SetupCommand extends BaseCommand {
         async (err,data) =>{
           if(err)throw err;
           let serverData = JSON.parse(data);
-          if(serverData?.[message.guild.name]){
+          let hasConfigFile = serverData?.[message.guild.name];
+          if(hasConfigFile){
             // If the discord server already has a config file
+            // We fetch that file
             let config = await fetchConfig(client);
             if(config){
             // If they are editing the current config file
-              createChannelsFromConfig(message,config);
+              createChannelsFromConfig(message,config,client);
               serverData[message.guild.name] = config;
               fs.writeFile(
                 "./serverData.json",
-                JSON.stringify(serverData,null,2),
-                console.log
-            )
+                JSON.stringify(serverData,null,2)),
+                  console.log
           }
             else{
             // If they are wanting to edit the current config file
