7a5e8f2fd0fb3d94d6aa7b32e92028df2c69b2a1
miaklwalker <ouisihai@gmail.com>
2022-08-05 01:04:33 -0500

    clean up

--- src/commands/watch/watch.command.js
+++ src/commands/watch/watch.command.js
@@ -18,14 +18,18 @@ module.exports = class WatchCommand extends BaseCommand {
                 let channel = message.channel;
 
                 if (isArkAlarmChannel) {
+                    // take t as num representing the number of minutes and convert that to milliseconds
                     let timeToMilliseconds = (t) => t * 60 * 1000;
+                    // take t as num representing the number of seconds and convert that to milliseconds
                     let timeToSeconds = (t) => t * 1000;
+                    // take t as num representing the number of milli and convert that to minutes
                     let timeToMinutes = (t) => t * 60;
                     let counter = 0;
 
                     let watchDuration =  args[0] || 1;
                     watchDuration = watchDuration < 15 ? 15 : watchDuration;
                     watchDuration = watchDuration > 1 ? watchDuration : 1;
+                    watchDuration = timeToMilliseconds(watchDuration);
 
                     let userInterval = args[1] ||  15;
                     let intervalDuration = userInterval < 15 ? 15 : userInterval;
@@ -48,7 +52,7 @@ module.exports = class WatchCommand extends BaseCommand {
                         embed = (await scanCluster(client, message))
                         embed.setTitle(`Cluster Scan Complete`)
                         await msgToEdit.edit(embed);
-                    }, timeToMilliseconds(watchDuration));
+                    }, watchDuration);
 
                 }
             }
