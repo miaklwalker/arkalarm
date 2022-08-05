e8b1dc21e08ac04311c89e21a5ab1be3a2d99e74
miaklwalker <ouisihai@gmail.com>
2022-08-04 23:17:52 -0500

    Added in a better user feedback system for watch progress

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -18,22 +18,33 @@ module.exports = class TestCommand extends BaseCommand {
             let channel = message.channel;
 
             if (isArkAlarmChannel) {
-                let time = args[0] || 5;
-                let timeInMilliseconds = (time) => time * 60 * 1000;
+                let timeToMilliseconds = (t) => t * 60 * 1000;
+                let timeToSeconds = (t) => t * 1000;
+                let timeToMinutes = (t) => t * 60;
 
+                let watchDuration =  1;
+                let userInterval = 15;
+                let counter = 0;
+                let intervalDuration = userInterval < 15 ? 15 : userInterval;
+                intervalDuration = timeToSeconds(intervalDuration);
                 let embed = await scanCluster(client, message);
                 let msgToEdit = await channel.send(embed);
 
                 let interval = setInterval(async () => {
                     console.log(`Scanning initiated by channel ${channel.name}`);
                     embed = (await scanCluster(client, message))
+                    let counterString = Math.floor(timeToMinutes(watchDuration)/userInterval)
+                    embed.setTitle(`Cluster Scan ${++counter} of ${counterString}`);
                     await msgToEdit.edit(embed);
-                }, 1000 * 30);
+                }, intervalDuration);
 
-                setTimeout(() => {
+                setTimeout(async () => {
                     console.log("Clearing interval");
                     clearInterval(interval);
-                }, timeInMilliseconds(time));
+                    embed = (await scanCluster(client, message))
+                    embed.setTitle(`Cluster Scan Complete`)
+                    await msgToEdit.edit(embed);
+                }, timeToMilliseconds(watchDuration));
 
             }
         }
